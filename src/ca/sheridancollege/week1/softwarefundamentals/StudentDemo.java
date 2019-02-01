738455d5153e5fe1ea6d82a171037d9d54e4b28a
Mohammad Shoeb <35189663+md-shoeb@users.noreply.github.com>
2019-02-01 15:51:21 -0500

    Adding variable demo
    Adding variable demo

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -14,6 +14,7 @@ public class StudentDemo
   private String studentID;
   private String name;
   private String course;
+  private String demo;
   
   /**
    * A constructor to pass in the given ID and then name
